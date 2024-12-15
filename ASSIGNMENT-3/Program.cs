ce6d8da36e87a4af692156784e601f15e1cb3983
Abdul Elrakh <abdulelrakh@gmail.com>
2024-12-15 20:32:16 +0100

    Aggiungere i file di progetto.

---
+++ ASSIGNMENT-3/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Numerics;
+
+class Assignment1
+{
+
+
+    static void Main(string[] args)
+    {
+        #region Check divisibility by 3 and 4
+        // Write a program that takes a number from the user then prints yes if that number can be divided by 3 and 4 otherwise print no.
+        // Example (1)
+        // Input: 12 
+        // Output: Yes
+        // Example (2)
+        // Input: 9 
+        // Output: No
+
+
+        #endregion
+
+        #region Check positive or negative number
+        // Write a program that allows the user to insert an integer then print negative if it is a negative number otherwise print positive.
+        // Example (1)
+        // Input: -5
+        // Output: negative
+        // Example (2)
+        // Input: 10
+        // Output: positive
+        #endregion
+
+        #region Find max and min element
+        // Write a program that takes 3 integers from the user then prints the max element and the min element.
+        // Example (1)
+        // Input: 7, 8, 5
+        // Output:
+        // max element = 8
+        // min element = 5
+        // Example (2)
+        // Input: 3, 6, 9
+        // Outputs:
+        // max element = 9
+        // min element = 3
+        #endregion
+
+        #region Check even or odd
+        // Write a program that allows the user to insert an integer number then checks if the number is even or odd.
+        #endregion
+
+        #region Check vowel or consonant
+        // Write a program that takes a character from the user then if it is a vowel (a, e, i, o, u) then print "vowel", otherwise print "consonant".
+        // Example (1)
+        // Input: O
+        // Output: vowel
+        // Example (2)
+        // Input: b
+        // Output: consonant
+        #endregion
+
+        #region Print numbers from 1 to n
+        // Write a program that allows the user to insert an integer then prints all numbers between 1 to that number.
+        // Example 
+        // Input: 5
+        // Output: 1, 2, 3, 4, 5
+        #endregion
+
+        #region Multiplication table
+        // Write a program that allows the user to insert an integer then prints a multiplication table up to 12.
+        // Example
+        // Input: 5
+        // Outputs: 5 10 15 20 25 30 35 40 45 50 55 60
+        #endregion
+
+        #region Print all even numbers
+        // Write a program that allows the user to insert a number then prints all even numbers between 1 and that number.
+        // Example:
+        // Input: 15
+        // Output: 2 4 6 8 10 12 14
+        #endregion
+
+        #region Calculate power of a number
+        // Write a program that takes two integers then prints the power.
+        // Example:
+        // Input: 4 3
+        // Output: 64
+        // Hint: How to calculate 4^3 = 4 * 4 * 4 = 64
+        #endregion
+
+        #region Calculate marks
+        // Write a program to enter marks of five subjects and calculate total, average, and percentage.
+        // Example
+        // Input: - Enter Marks of five subjects: 95 76 58 90 89
+        // Output: Total marks = 408
+        //         Average marks = 81
+        //         Percentage = 81
+        #endregion
+
+        #region Days in month
+        // Write a program to input the month number and print the number of days in that month.
+        // Example
+        // Input: Month Number: 1
+        // Output: Days in Month: 31
+        #endregion
+
+        #region Simple calculator
+        // Write a program to create a simple calculator.
+        #endregion
+
+        #region Reverse a string
+        // Write a program to allow the user to enter a string and print the reverse of it.
+        #endregion
+
+        #region Reverse an integer
+        // Write a program to allow the user to enter an integer and print the reverse of it.
+        #endregion
+
+        #region Find prime numbers in a range
+        // Write a program in C# Sharp to find prime numbers within a range of numbers.
+        // Test Data:
+        // Input starting number of range: 1
+        // Input ending number of range : 50
+        // Expected Output:
+        // The prime numbers between 1 and 50 are:
+        // 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+        #endregion
+
+        #region Convert decimal to binary
+        // Write a program in C# Sharp to convert a decimal number into binary without using an array.
+        // Test Data:
+        // Enter a number to convert: 25
+        // Expected Output:
+        // The binary of 25 is 11001.
+        #endregion
+
+        #region Check collinearity of points
+        // Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line.
+        #endregion
+
+        #region Evaluate worker efficiency
+        // Within a company, the efficiency of workers is evaluated based on the duration required to complete a specific task. A worker's efficiency level is determined as follows:
+        // - If the worker completes the job within 2 to 3 hours, they are considered highly efficient.
+        // - If the worker takes 3 to 4 hours, they are instructed to increase their speed.
+        // - If the worker takes 4 to 5 hours, they are provided with training to enhance their speed.
+        // - If the worker takes more than 5 hours, they are required to leave the company.
+        // To calculate the efficiency of a worker, the time taken for the task is obtained via user input.
+        #endregion
+
+        #region Print identity matrix
+        // Write a program that prints an identity matrix using a for loop. In other words, take a value n from the user and show the identity matrix of size n * n.
+        #endregion
+
+        #region Sum of array elements
+        // Write a program in C# Sharp to find the sum of all elements of the array.
+        #endregion
+
+
+    }
+}
